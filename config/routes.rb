e93f4be6eab0820739e7da4ced68766ee79883d4
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-13 17:44:35 +0300

    Add the path route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      post 'users/register', to: 'users#create'
+    end
+  end
 end
