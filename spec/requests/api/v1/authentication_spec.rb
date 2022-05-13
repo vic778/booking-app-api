c04c6626ebd1d9e9d27e70545c94259524a63683
cwaku <rickymormor1@gmail.com>
2022-05-13 18:45:13 +0000

    Test api login

--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Authentications", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /api/v1/authentications" do
+    before(:each) do
+      @user = FactoryBot.create(:user)
+
+      post '/api/v1/auth/login', params: {
+        email: @user.email,
+        password: @user.password
+      }
+      @token = JSON.parse(response.body)['auth_token']
+
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+      end
+
+    it "returns the user's auth_token" do
+      expect(JSON.parse(response.body)['auth_token']).to_not be_nil
+      end
+
+    it "returns the user's email" do
+      expect(JSON.parse(response.body)['email']).to_not be_nil
+      end
+
+    it "returns the user's name" do
+      expect(JSON.parse(response.body)['name']).to_not be_nil
+      end
   end
 end
