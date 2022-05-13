d37e0d494d75f7796fe6f299ef3232d6733e1b12
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-13 17:45:04 +0300

    Create the Api folder that contains the modul for user

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::UsersController < ApplicationController
+    before_action :set_user, only: [:update, :destroy]
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            token = encode_token({ user_id: @user.id })
+            render json: {success: true, message: "User created successfully"}, status: :created
+        else
+            render json: {success: false, message: @user.errors.full_messages}, status: :bad_request
+        end
+    end
+
+    def update
+        # @user = User.find(params[:id])
+        if @user.update(user_params)
+            render json: {success: true, message: "User updated successfully"}, status: :ok
+        else
+            render json: {success: false, message: @user.errors.full_messages}, status: :bad_request
+        end
+    end
+
+    def destroy
+        # @user = User.find(params[:id])
+        @user.destroy
+        render json: {success: true, message: "User deleted successfully"}, status: :ok
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+end
