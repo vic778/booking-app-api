3a3e65e54b0d0166906dfeeff9cd4561159c363f
cwaku <rickymormor1@gmail.com>
2022-05-13 16:03:31 +0000

    Fix Linter errors

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AuthenticationController < ApplicationController
   def login
     @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
+    if @user&.authenticate(params[:password])
       token = encode_token({ user_id: @user.id })
       render json: { user: @user, jwt: token }, status: :accepted
     else
