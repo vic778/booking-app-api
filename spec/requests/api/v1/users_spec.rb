c826e03ca58859cd500e50a2bfc29124204cca36
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-14 15:53:32 +0300

    Test user_spec

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,42 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
-  
-  # describe "GET /api/v1/users" do
-  #   it "returns http success" do
-  #     get "/api/v1/users"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  
-  # describe "GET /api/v1/users/:id" do
-  #   it "returns http success" do
-  #     get "/api/v1/users/1"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  
-  describe "POST /api/v1/users/register" do
-    it "returns http success" do
-      post "/api/v1/users/register", params: {
-          name: "Rails",
-          email: "rails@", 
-          password: "Railsrails",
-          password_confirmation: "Railsrails"
-      }
-      expect(response).to have_http_status(:success)
-    end
+  it 'it create a new user' do
+    user_params = {
+      name: 'Victor',
+      email: 'barhvictor@gmail.com',
+      password: '123456',
+      password_confirmation: '123456'
+    }
+    post '/api/v1/users/register', params: user_params
+    expect(response).to have_http_status(:created)
   end
 
-  # describe "PUT /api/v1/users/:id" do
-  #   it "returns http success" do
-  #     put "/api/v1/users/1", params: {   
-  #         name: "Rails",
-  #         password: "Railsrails",
-  #         password_confirmation: "Railsrails"
-  #     }
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  it 'it should return error when create a new user' do
+    user_params = {
+      name: 'Victor',
+      email: '',
+      password: '123456',
+      password_confirmation: '123456'
+    }
+    post '/api/v1/users/register', params: user_params
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+
+  it "it should update a user" do
+    user = FactoryBot.create(:user)
+    user_params = {
+      name: 'Barh',
+      password: '123456',
+      password_confirmation: '123456'
+    }
+    put "/api/v1/users/#{user.id}", params: user_params
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "it should return error when update a user" do
+    user = FactoryBot.create(:user)
+    user_params = {
+      name: 'Barh',
+      password: '123456',
+      password_confirmation: ''
+    }
+    put "/api/v1/users/#{user.id}", params: user_params
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
 
 end
