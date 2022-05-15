ae06fbcbd3677044509af1d41545e7da10f0773c
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-15 22:33:54 +0300

    Add the association

---
+++ app/models/motorcycle.rb
@@ -0,0 +1,11 @@
+class Motorcycle < ApplicationRecord
+    belongs_to :user
+
+    validates :title, presence: true
+    validates :description, presence: true
+   
+    validates :year, presence: true
+    validates :model, presence: true
+    validates :color, presence: true
+
+end
