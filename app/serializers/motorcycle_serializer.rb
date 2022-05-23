75548ded6e059f42b3be22d108f7e09fb671efd7
cwaku <rickymormor1@gmail.com>
2022-05-23 10:29:29 +0000

    Fix linter errors

--- app/serializers/motorcycle_serializer.rb
+++ app/serializers/motorcycle_serializer.rb
@@ -1,5 +1,5 @@
 class MotorcycleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description,  :model, :year, :color, :image, :available, :created_at, :updated_at
+  attributes :id, :title, :description, :model, :year, :color, :image, :available, :created_at, :updated_at
 
   has_many :reservations
 end
