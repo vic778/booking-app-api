32593474249a8898ad5a4c3d21d58aa5ad3c15da
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-27 10:04:24 +0300

    Update available byr false after reserving a bik

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,10 +1,16 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :motorcycle
+  after_save :set_available
 
   validates :motorcycle_id, presence: true, uniqueness: true
   validates :duration, presence: true
   validates :price, presence: true
   validates :date, presence: true
   validates :city, presence: true
+
+  def set_available
+    @motorcycle = Motorcycle.find(self.motorcycle_id)
+    @motorcycle.update(available: false)
+  end
 end
