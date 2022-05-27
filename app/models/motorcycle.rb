42f48b8d4e2bca17cfd2a4678fee3ed6b1610c73
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-27 10:03:41 +0300

    Add dependes destroy for a reservation

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,19 +1,13 @@
 class Motorcycle < ApplicationRecord
-  after_save :set_available
-
   belongs_to :user
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   mount_uploader :image, ImageUploader
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
   validates :description, presence: true
 
   validates :year, presence: true
-  validates :model, presence: true
+  validates :model, presence: true, uniqueness: true
   validates :color, presence: true
   validates :image, presence: true
-
-  def set_available
-    self.update(available: false)
-  end
 end
