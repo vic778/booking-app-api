c59c41ed62612802b5ec2a4df1e41bcb3ef56e96
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-08 09:48:05 +0300

    Fix scooter bugg

--- app/controllers/api/v2/motorcyles_controller.rb
+++ app/controllers/api/v2/motorcyles_controller.rb
@@ -15,34 +15,34 @@ class Api::V2::MotorcylesController < ApplicationController
   def create
     @motorcycle = Motorcycle.new(motorcycle_params.merge(user: @user))
     if @motorcycle.save
-      render json: { success: true, message: "Motorcycle created successfully", motorcycle: @motorcycle },
+      render json: { success: true, message: "Scooter created successfully", motorcycle: @motorcycle },
              status: :created
     else
-      render json: { success: false, message: "Motorcycle not created", errors: @motorcycle.errors },
+      render json: { success: false, message: "Scooter not created", errors: @motorcycle.errors },
              status: :bad_request
     end
   end
 
   def update
     if @motorcycle.update(motorcycle_params)
-      render json: { success: true, message: "Motorcycle updated successfully", motorcycle: @motorcycle },
+      render json: { success: true, message: "Scooter updated successfully", motorcycle: @motorcycle },
              status: :ok
     else
-      render json: { success: false, message: "Motorcycle not updated", errors: @motorcycle.errors },
+      render json: { success: false, message: "Scooter not updated", errors: @motorcycle.errors },
              status: :bad_request
     end
   end
 
   def destroy
-    if motorcycle = @user.motorcycles.find_by(id: params[:id])
-      if motorcycle.available == true
-        @reservation.destroy
-        render json: { success: true, message: "Reservation deleted successfully" }, status: :ok
+    if @motorcycle = @user.motorcycles.find(params[:id])
+      if @motorcycle.available == true
+        @motorcycle.destroy
+        render json: { success: true, message: "Scooter deleted successfully" }, status: :ok
       else
-        render json: { success: false, message: "Motorcycle is reserved" }, status: :bad_request
+        render json: { success: false, message: "Scooter is reserved" }, status: :bad_request
       end
     else
-      render json: { success: false, message: "Motorcycle not found" }, status: 404
+      render json: { success: false, message: "Only the owner can delete this scooter" }, status: 404
     end
   end
 
